dbc190d27c166ef66fac6dd4f381cb4147536c5c
Ni9htOw1 <boltrushko.ol@gmail.com>
2022-04-12 19:41:37 +0300

    <Requer>

--- Laba 5/Laba 5/Program.cs
+++ Laba 5/Laba 5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Math;
+Console.WriteLine("Введите x, точность:");
+double x = .0;
+double e = .0;
+x = Convert.ToDouble(Console.ReadLine());
+e = Convert.ToDouble(Console.ReadLine());
+double s = .0;
+double sl = 1.0;
+int n = 0;
+while (Abs(sl) > e)
+{
+	s += sl;
+	n++;
+	sl *= x * x /(2*n+2)*(2 * n +1);
+}
+Console.WriteLine($"Сумма:{s}");
