18c0709191a15c5f9ed81fa37f456a2936e9e348
Ni9htOw1 <boltrushko.ol@gmail.com>
2022-04-12 20:43:04 +0300

    <Required>

---
+++ Laba 7/Laba 7/Program.cs
@@ -0,0 +1,17 @@
+﻿StreamReader read = new("Inlet.txt");
+int n = Convert.ToInt32(read.ReadLine()); ;
+int[] mas_a = new int[n];
+for (int i = 0; i < n; i++)
+{
+	mas_a[i] = Convert.ToInt32(read.ReadLine());
+}
+read.Close();
+int k=0;
+for (int i = 0; i < n; i++)
+{
+	if (mas_a[i]%2==1) k++;
+}
+StreamWriter write = new("Outlet.txt");
+write.WriteLine(k);
+write.Close();
+
